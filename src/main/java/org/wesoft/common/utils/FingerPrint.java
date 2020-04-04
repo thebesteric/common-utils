fe5f4950e8eaf52484748f658ca367587b50b020
eric <whatisjava@hotmail.com>
2020-04-04 17:26:40 +0800

    replace

--- src/main/java/org/wesoft/common/utils/FingerPrint.java
+++ src/main/java/org/wesoft/common/utils/FingerPrint.java
@@ -2,13 +2,13 @@ package org.wesoft.common.utils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.color.ColorSpace;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorConvertOp;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Arrays;
 
 /**
  * 图片指纹工具类
@@ -20,206 +20,194 @@ import java.util.Arrays;
  */
 public class FingerPrint {
 
-    public static void main(String[] args) {
-        try {
-            BufferedImage bufferedImage = ImageIO.read(new URL("http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLam81BZbibbN4qjvyXNC62yaalIy7pFFpZT4192SVB96xmcic3EVnxrjmaRbCpG2aXHKRplzuJiaPuQ/132"));
-            FingerPrint fp0 = new FingerPrint(bufferedImage);
-            FingerPrint fp1 = new FingerPrint(ImageIO.read(new File("d:\\1.jpg")));
-            // FingerPrint fp2 = new FingerPrint(ImageIO.read(new File("d:\\2.jpg")));
-            System.out.println(fp0.toString(true));
-            System.out.println(fp1.toString(true));
+    private static final int WIDTH = 8, HEIGHT = 8;
 
-            System.out.printf("sim=%f", fp1.compare(fp0));
-        } catch (IOException e) {
+    private static final String HEX_STR = "0123456789ABCDEF";
+
+    private String hashCode;
+
+    public FingerPrint(String filename) {
+        this(readImage(filename));
+    }
+
+    public FingerPrint(BufferedImage source) {
+
+        // 第一步，缩小尺寸。
+        // 将图片缩小到 8x8 的尺寸，总共 64 个像素。
+        // 这一步的作用是去除图片的细节，只保留结构、明暗等基本信息，摒弃不同尺寸、比例带来的图片差异。
+        BufferedImage thumb = thumbnails(source, WIDTH, HEIGHT, false);
+
+        // 第二步，简化色彩。
+        // 将缩小后的图片，转为 64 级灰度。也就是说，所有像素点总共只有64种颜色。
+        int[] pixels = new int[WIDTH * HEIGHT];
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                pixels[i * HEIGHT + j] = rgbToGray(thumb.getRGB(i, j));
+            }
+        }
+
+        // 第三步，计算平均值。
+        // 计算所有64个像素的灰度平均值。
+        int avgPixel = average(pixels);
+
+        // 第四步，比较像素的灰度。
+        // 将每个像素的灰度，与平均值进行比较。大于或等于平均值，记为 1；小于平均值，记为 0
+        int[] comps = new int[WIDTH * HEIGHT];
+        for (int i = 0; i < comps.length; i++) {
+            if (pixels[i] >= avgPixel) {
+                comps[i] = 1;
+            } else {
+                comps[i] = 0;
+            }
+        }
+
+        // 第五步，计算哈希值。
+        // 将上一步的比较结果，组合在一起，就构成了一个64位的整数，这就是这张图片的指纹。组合的次序并不重要，只要保证所有图片都采用同样次序就行了。
+        StringBuilder hashCode = new StringBuilder();
+        for (int comp : comps) {
+            hashCode.append(binaryToHex(comp));
+        }
+
+        // 得到指纹以后，就可以对比不同的图片，看看64位中有多少位是不一样的。
+        this.hashCode = hashCode.toString();
+    }
+
+    /**
+     * 读取图片
+     *
+     * @param filename filename
+     */
+    public static BufferedImage readImage(String filename) {
+        BufferedImage sourceImage = null;
+        try {
+            File inputFile = new File(filename);
+            sourceImage = ImageIO.read(inputFile);
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        return sourceImage;
     }
 
-    /** 图像指纹的尺寸, 将图像 resize 到指定的尺寸，来计算哈希数组 */
-    private static final int HASH_SIZE = 16;
+    public static BufferedImage thumbnails(BufferedImage source, int width, int height, boolean b) {
+        int type = source.getType();
+        BufferedImage target;
+        double sx = (double) width / source.getWidth();
+        double sy = (double) height / source.getHeight();
 
-    /** 保存图像指纹的二值化矩阵 */
-    private final byte[] binaryzationMatrix;
-
-    public FingerPrint(byte[] hashValue) {
-        if (hashValue.length != HASH_SIZE * HASH_SIZE)
-            throw new IllegalArgumentException(String.format(
-                    "length of hashValue must be %d", HASH_SIZE * HASH_SIZE));
-        this.binaryzationMatrix = hashValue;
-    }
-
-    public FingerPrint(String hashValue) {
-        this(toBytes(hashValue));
-    }
-
-    public FingerPrint(BufferedImage src) {
-        this(hashValue(src));
-    }
-
-    private static byte[] hashValue(BufferedImage src) {
-        BufferedImage hashImage = resize(src, HASH_SIZE, HASH_SIZE);
-        byte[] matrixGray = (byte[]) toGray(hashImage).getData()
-                .getDataElements(0, 0, HASH_SIZE, HASH_SIZE, null);
-        return binaryzation(matrixGray);
-    }
-
-    /**
-     * 从压缩格式指纹创建{@link FingerPrint}对象
-     *
-     * @param compactValue 压缩值
-     */
-    public static FingerPrint createFromCompact(byte[] compactValue) {
-        return new FingerPrint(unCompact(compactValue));
-    }
-
-    public static boolean validHashValue(byte[] hashValue) {
-        if (hashValue.length != HASH_SIZE)
-            return false;
-        for (byte b : hashValue) {
-            if (0 != b && 1 != b)
-                return false;
-        }
-        return true;
-    }
-
-    public static boolean validHashValue(String hashValue) {
-        if (hashValue.length() != HASH_SIZE)
-            return false;
-        for (int i = 0; i < hashValue.length(); ++i) {
-            if ('0' != hashValue.charAt(i) && '1' != hashValue.charAt(i))
-                return false;
-        }
-        return true;
-    }
-
-    public byte[] compact() {
-        return compact(binaryzationMatrix);
-    }
-
-    /**
-     * 指纹数据按位压缩
-     *
-     * @param hashValue hash 值
-     */
-    private static byte[] compact(byte[] hashValue) {
-        byte[] result = new byte[(hashValue.length + 7) >> 3];
-        byte b = 0;
-        for (int i = 0; i < hashValue.length; ++i) {
-            if (0 == (i & 7)) {
-                b = 0;
-            }
-            if (1 == hashValue[i]) {
-                b |= 1 << (i & 7);
-            } else if (hashValue[i] != 0)
-                throw new IllegalArgumentException(
-                        "invalid hashValue,every element must be 0 or 1");
-            if (7 == (i & 7) || i == hashValue.length - 1) {
-                result[i >> 3] = b;
+        if (b) {
+            if (sx > sy) {
+                sx = sy;
+                width = (int) (sx * source.getWidth());
+            } else {
+                sy = sx;
+                height = (int) (sy * source.getHeight());
             }
         }
-        return result;
-    }
 
-    /**
-     * 压缩格式的指纹解压缩
-     *
-     * @param compactValue 压缩值
-     */
-    private static byte[] unCompact(byte[] compactValue) {
-        byte[] result = new byte[compactValue.length << 3];
-        for (int i = 0; i < result.length; ++i) {
-            if ((compactValue[i >> 3] & (1 << (i & 7))) == 0)
-                result[i] = 0;
-            else
-                result[i] = 1;
-        }
-        return result;
-    }
-
-    /**
-     * 字符串类型的指纹数据转为字节数组
-     *
-     * @param hashValue 哈希值
-     */
-    private static byte[] toBytes(String hashValue) {
-        hashValue = hashValue.replaceAll("\\s", "");
-        byte[] result = new byte[hashValue.length()];
-        for (int i = 0; i < result.length; ++i) {
-            char c = hashValue.charAt(i);
-            if ('0' == c)
-                result[i] = 0;
-            else if ('1' == c)
-                result[i] = 1;
-            else
-                throw new IllegalArgumentException("invalid hashValue String");
-        }
-        return result;
-    }
-
-    /**
-     * 缩放图像到指定尺寸
-     *
-     * @param src    图片
-     * @param width  宽度
-     * @param height 高度
-     * @return BufferedImage
-     */
-    private static BufferedImage resize(Image src, int width, int height) {
-        BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-        Graphics g = result.getGraphics();
-        try {
-            g.drawImage(src.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);
-        } finally {
-            g.dispose();
-        }
-        return result;
-    }
-
-    /**
-     * 计算均值
-     *
-     * @param src src
-     */
-    private static int mean(byte[] src) {
-        long sum = 0;
-        // 将数组元素转为无符号整数
-        for (byte b : src)
-            sum += (long) b & 0xff;
-        return (int) (Math.round((float) sum / src.length));
-    }
-
-    /**
-     * 二值化处理
-     *
-     * @param src src
-     */
-    private static byte[] binaryzation(byte[] src) {
-        byte[] dst = src.clone();
-        int mean = mean(src);
-        for (int i = 0; i < dst.length; ++i) {
-            // 将数组元素转为无符号整数再比较
-            dst[i] = (byte) (((int) dst[i] & 0xff) >= mean ? 1 : 0);
-        }
-        return dst;
-
-    }
-
-    /**
-     * 转灰度图像
-     *
-     * @param src src
-     */
-    private static BufferedImage toGray(BufferedImage src) {
-        if (src.getType() == BufferedImage.TYPE_BYTE_GRAY) {
-            return src;
+        if (type == BufferedImage.TYPE_CUSTOM) {
+            ColorModel cm = source.getColorModel();
+            WritableRaster raster = cm.createCompatibleWritableRaster(width, height);
+            boolean alphaPremultiplied = cm.isAlphaPremultiplied();
+            target = new BufferedImage(cm, raster, alphaPremultiplied, null);
         } else {
-            // 图像转灰
-            BufferedImage grayImage = new BufferedImage(src.getWidth(),
-                    src.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-            new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null)
-                    .filter(src, grayImage);
-            return grayImage;
+            target = new BufferedImage(width, height, type);
         }
+
+        Graphics2D g = target.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.drawRenderedImage(source, AffineTransform.getScaleInstance(sx, sy));
+        g.dispose();
+        return target;
+    }
+
+    /**
+     * 二进制转为十六进制
+     *
+     * @param binary binary
+     */
+    private static char binaryToHex(int binary) {
+        char ch;
+        switch (binary) {
+            case 0:
+                ch = '0';
+                break;
+            case 1:
+                ch = '1';
+                break;
+            case 2:
+                ch = '2';
+                break;
+            case 3:
+                ch = '3';
+                break;
+            case 4:
+                ch = '4';
+                break;
+            case 5:
+                ch = '5';
+                break;
+            case 6:
+                ch = '6';
+                break;
+            case 7:
+                ch = '7';
+                break;
+            case 8:
+                ch = '8';
+                break;
+            case 9:
+                ch = '9';
+                break;
+            case 10:
+                ch = 'a';
+                break;
+            case 11:
+                ch = 'b';
+                break;
+            case 12:
+                ch = 'c';
+                break;
+            case 13:
+                ch = 'd';
+                break;
+            case 14:
+                ch = 'e';
+                break;
+            case 15:
+                ch = 'f';
+                break;
+            default:
+                ch = ' ';
+        }
+        return ch;
+    }
+
+    /**
+     * 灰度值计算
+     *
+     * @param pixels 像素
+     * @return int 灰度值
+     */
+    private static int rgbToGray(int pixels) {
+        int _red = (pixels >> 16) & 0xFF;
+        int _green = (pixels >> 8) & 0xFF;
+        int _blue = (pixels) & 0xFF;
+        return (int) (0.3 * _red + 0.59 * _green + 0.11 * _blue);
+    }
+
+    /**
+     * 计算数组的平均值
+     *
+     * @param pixels 数组
+     * @return int 平均值
+     */
+    private static int average(int[] pixels) {
+        float m = 0;
+        for (int pixel : pixels) {
+            m += pixel;
+        }
+        m = m / pixels.length;
+        return (int) m;
     }
 
     @Override
@@ -227,108 +215,50 @@ public class FingerPrint {
         return toString(false);
     }
 
-    /**
-     * 转 String 表现形式
-     *
-     * @param multiLine 是否分行
-     */
     public String toString(boolean multiLine) {
-        StringBuffer buffer = new StringBuffer();
-        int count = 0;
-        for (byte b : this.binaryzationMatrix) {
-            buffer.append(0 == b ? '0' : '1');
-            if (multiLine && ++count % HASH_SIZE == 0)
+        if (!multiLine) {
+            return this.hashCode;
+        }
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 1; i <= this.hashCode.length(); i++) {
+            buffer.append(hashCode.charAt(i - 1));
+            if (i % 8 == 0)
                 buffer.append('\n');
         }
         return buffer.toString();
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof FingerPrint) {
-            return Arrays.equals(this.binaryzationMatrix,
-                    ((FingerPrint) obj).binaryzationMatrix);
-        } else
-            return super.equals(obj);
-    }
-
     /**
-     * 与指定的压缩格式指纹比较相似度
+     * 计算"汉明距离"（Hamming distance）。
+     * 如果不相同的数据位不超过 5，就说明两张图片很相似；如果大于 10，就说明这是两张不同的图片。
      *
-     * @param compactValue 压缩值
-     * @see #compare(FingerPrint)
+     * @param sourceHashCode sourceHashCode
+     * @param targetHashCode targetHashCode
      */
-    public float compareCompact(byte[] compactValue) {
-        return compare(createFromCompact(compactValue));
-    }
-
-    /**
-     * 比较
-     *
-     * @param hashValue 哈希值
-     * @see #compare(FingerPrint)
-     */
-    public float compare(String hashValue) {
-        return compare(new FingerPrint(hashValue));
-    }
-
-    /**
-     * 与指定的指纹比较相似度
-     *
-     * @param hashValue 哈希值
-     * @see #compare(FingerPrint)
-     */
-    public float compare(byte[] hashValue) {
-        return compare(new FingerPrint(hashValue));
-    }
-
-    /**
-     * 与指定图像比较相似度
-     *
-     * @param image 图片
-     * @see #compare(FingerPrint)
-     */
-    public float compare(BufferedImage image) {
-        return compare(new FingerPrint(image));
-    }
-
-    /**
-     * 比较指纹相似度
-     *
-     * @param src src
-     * @see #compare(byte[], byte[])
-     */
-    public float compare(FingerPrint src) {
-        if (src.binaryzationMatrix.length != this.binaryzationMatrix.length)
-            throw new IllegalArgumentException(
-                    "length of hashValue is mismatch");
-        return compare(binaryzationMatrix, src.binaryzationMatrix);
-    }
-
-    /**
-     * 判断两个数组相似度，数组长度必须一致否则抛出异常
-     *
-     * @param f1 数组1
-     * @param f2 数组2
-     * @return 返回相似度 ( 0.0 ~ 1.0 )
-     */
-    public static float compare(byte[] f1, byte[] f2) {
-        if (f1.length != f2.length)
-            throw new IllegalArgumentException("mismatch FingerPrint length");
-        int sameCount = 0;
-        for (int i = 0; i < f1.length; ++i) {
-            if (f1[i] == f2[i])
-                ++sameCount;
+    public static int hammingDistance(String sourceHashCode, String targetHashCode) {
+        int difference = 0;
+        int len = sourceHashCode.length();
+        for (int i = 0; i < len; i++) {
+            if (sourceHashCode.charAt(i) != targetHashCode.charAt(i)) {
+                difference++;
+            }
         }
-        return (float) sameCount / f1.length;
+        return difference;
     }
 
-    public static float compareCompact(byte[] f1, byte[] f2) {
-        return compare(unCompact(f1), unCompact(f2));
-    }
+    public static void main(String[] args) {
+        try {
+            FingerPrint fp0 = new FingerPrint(ImageIO.read(new URL("http://thirdwx.qlogo.cn/mmopen/eLylRtUHQibVjQcuTA6qTiamRV9vzPSx6624AGQt8df2BibFXU5icKiachibG0FSbcTCxxJAzzN2TKzOwOticmT6B3VxcQtlshRyUw0/132")));
+            FingerPrint fp1 = new FingerPrint(ImageIO.read(new URL("http://wx.qlogo.cn/mmhead/ver_1/SkDOvWIeFFcnxkGwv5WIXO8Bsck8zB9Ekye4Zial5OOUo94micVsSulCoTht8spLiaAhI2Kq3qdYFQiaNgOfBHgkIqrxibOUDugZV2wt9f2m2Zsw/132")));
+            System.out.println(fp0.toString(true));
+            System.out.println(fp1.toString(true));
 
-    public static float compare(BufferedImage image1, BufferedImage image2) {
-        return new FingerPrint(image1).compare(new FingerPrint(image2));
+            int difference = hammingDistance(fp0.toString(), fp1.toString());
+            System.out.print("汉明距离:" + difference);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
